81088accbaf31197342d6a3300d158b64caebee6
Jim Vickery <jimrv8@yahoo.com>
2018-11-11 02:12:57 -0600

    added border around image

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,28 +1,49 @@
 import React, { PropTypes } from 'react'; 
 import { 
   StyleSheet, 
-  Image 
-} from 'react-native';
+  Image, 
+  View 
+} from 'react-native'; 
+import Title from './Title'; 
+ 
+const Thumbnail = ({ url, titleText, accentColor, style }) => { 
+  const imageStyle = { 
+    backgroundColor: `${accentColor}77` // adds some transparency to the color 
+  }; 
+ 
+  return (
 
-const Thumbnail = ({ url }) => { 
-  return ( 
-    <Image 
-      style={[styles.image]} 
-      source={{ 
-        uri: url 
-      }} 
-    /> 
+<View style={[styles.container, { borderColor: accentColor }, style]}> 
+      <Image 
+        style={[styles.image]} 
+        source={{ 
+          uri: url 
+        }} 
+      > 
+        <Title style={styles.title}>{titleText}</Title> 
+      </Image> 
+    </View> 
   ); 
 }; 
  
 Thumbnail.propTypes = { 
-  url: PropTypes.string.isRequired 
+  style: View.propTypes.style, 
+  url: PropTypes.string.isRequired, 
+  titleText: PropTypes.string, 
+  accentColor: PropTypes.string.isRequired 
 }; 
  
 const styles = StyleSheet.create({ 
+  container: { 
+    borderBottomWidth: 3, 
+    borderStyle: 'solid' 
+  }, 
   image: { 
-    height: 100, 
+    height: 100,
     justifyContent: 'flex-end' 
+  }, 
+  title: { 
+    padding: 5 
   } 
 }); 
  
