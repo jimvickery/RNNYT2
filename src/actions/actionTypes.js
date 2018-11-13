c1df53b5452c6f6bb3f88fd233689dbbb09c01da
Jim Vickery <jimrv8@yahoo.com>
2018-11-12 23:52:39 -0600

    acced actionTypes.js

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,4 @@
+export const LOAD_NEWS = 'LOAD_NEWS'; 
+export const SEARCH_NEWS = 'SEARCH_NEWS';
+
+Masiello, Eric. Mastering React Native (Kindle Location 3318). Packt Publishing. Kindle Edition. 
