b857b7f314a5dc0be36016678d3ff9c68188e2f2
Jim Vickery <jimrv8@yahoo.com>
2018-11-13 21:59:45 -0600

    made change to NewsFeedContainer

--- src/config/containers/NewsFeedContainer.js
+++ src/config/containers/NewsFeedContainer.js
@@ -1,7 +1,9 @@
 import { connect } from 'react-redux'; 
 import { bindActionCreators } from 'redux'; 
 import { loadNews } from '../actions/newsActions'; 
-import NewsFeed from '../components/NewsFeed'; 
+// import NewsFeed from '../components/NewsFeed'; 
+import NewsFeedContainer from '../containers/NewsFeedContainer';
+
  
 const mapStateToProps = state => ({ 
   news: state.news 
