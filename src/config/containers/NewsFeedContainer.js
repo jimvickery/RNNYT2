06743cfb7eb662afa695ac4e5dfa06eda81ece8b
Jim Vickery <jimrv8@yahoo.com>
2018-11-15 01:11:08 -0600

    added reshapenewsdata to Newsfeed.js

--- src/config/containers/NewsFeedContainer.js
+++ src/config/containers/NewsFeedContainer.js
@@ -3,12 +3,14 @@ import { bindActionCreators } from 'redux';
 import { loadNews } from '../actions/newsActions'; 
 // import NewsFeed from '../components/NewsFeed'; 
 import NewsFeedContainer from '../containers/NewsFeedContainer';
+import { reshapeNewsData } from '../util/dataTransformations';
 
  
 const mapStateToProps = state => ({ 
-  news: state.news 
-}); 
- 
+  news: reshapeNewsData(state.news) 
+});
+
+
 const mapDispatchToProps = dispatch => ( 
   bindActionCreators({
 
